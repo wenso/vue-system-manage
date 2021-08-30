c4abd49b50ffea88c4ff3000ca36f3d450fa1f23
os-zhaoyong <zhaoyong.neu@neusoft.com>
2021-08-30 16:20:44 +0800

    初始化项目

--- src/mock/modules/menu.js
+++ src/mock/modules/menu.js
@@ -1,32 +1,172 @@
+//上线的menu-manage需要在路径中增加项目名命名空间，区别不同项目或租户的情况
 const data=[
     {
         path:'/',
-        compnent:'Layout',
-        redirect:'/dashboard/',
-        name:'layout',
+        redirect:'/dashboard',
+        props:{
+          menuPath:'/dashboard'
+        },
         meta:{
             title:'系统首页',
             index:1,
             icon:''
         },
+        menuId:'1',
         children:[
             {
                 path:'dashboard',
-                name:'dashboard',
-                component:'/dashboard/index',
+                props:{
+                    menuPath:'/dashboard'
+                },
                 meta:{
-                    title:'统计看板'
-                }
+                    title:'统计看板',
+                    index:1,
+                    icon:''
+                },
+                menuId:'2'
             },{
-                path:'',
-                name:'',
-                component:'',
+                path:'account-manage',
+                props:{
+                    menuPath:'/account-manage',
+                },
                 meta:{
-                    title:''
-                }
+                    title:'用户中心',
+                    index:1,
+                    icon:''
+                },
+                menuId:'3',
+                children:[{
+                        path: 'edit',
+                        props: {
+                            menuPath: '/account-manage/edit'
+                        },
+                        meta: {
+                            title: '用户编辑',
+                            index:1,
+                            icon:''
+                        },
+                        menuId: '4'
+                    },{
+                        path: 'add',
+                        props: {
+                            menuPath: '/account-manage/add'
+                        },
+                        meta: {
+                            title: '用户添加',
+                            index:1,
+                            icon:''
+                        },
+                        menuId: '5'
+                    }
+                ]
+            },{
+                path:'role-manage',
+                props:{
+                    menuPath:'/role-manage',
+                },
+                meta:{
+                    title:'角色管理',
+                    index:1,
+                    icon:''
+                },
+                menuId:'',
+                children:[{
+                        path: 'edit',
+                        props: {
+                            menuPath: '/role-manage/edit'
+                        },
+                        meta: {
+                            title: '角色编辑',
+                            index:1,
+                            icon:''
+                        },
+                        menuId: '6'
+                    },{
+                        path: 'add',
+                        props: {
+                            menuPath: '/role-manage/add'
+                        },
+                        meta: {
+                            title: '角色添加',
+                            index:1,
+                            icon:''
+                        },
+                        menuId: '7'
+                    }
+                ]
+            },{
+                path:'role-manage',
+                props:{
+                    menuPath:'/role-manage',
+                },
+                meta:{
+                    title:'角色管理',
+                    index:1,
+                    icon:''
+                },
+                menuId:'8',
+                children:[{
+                        path: 'edit',
+                        props: {
+                            menuPath: '/role-manage/edit'
+                        },
+                        meta: {
+                            title: '角色编辑',
+                            index:1,
+                            icon:''
+                        },
+                        menuId: '9'
+                    },{
+                        path: 'add',
+                        props: {
+                            menuPath: '/role-manage/add'
+                        },
+                        meta: {
+                            title: '角色添加',
+                            index:1,
+                            icon:''
+                        },
+                        menuId: '10'
+                    }
+                ]
+            },{
+                path:'menu-manage',
+                props:{
+                    menuPath:'/menu-manage',
+                },
+                meta:{
+                    title:'菜单管理',
+                    index:1,
+                    icon:''
+                },
+                menuId:'11',
+                children:[{
+                        path: 'edit',
+                        props: {
+                            menuPath: '/menu-manage/edit'
+                        },
+                        meta: {
+                            title: '菜单编辑',
+                            index:1,
+                            icon:''
+                        },
+                        menuId: '12'
+                    },{
+                        path: 'add',
+                        props: {
+                            menuPath: '/menu-manage/add'
+                        },
+                        meta: {
+                            title: '菜单添加',
+                            index:1,
+                            icon:''
+                        },
+                        menuId: '13'
+                    }
+                ]
             }
         ]
-    }
+    },
 ]
 const menuApi=[]
 export default menuApi;
