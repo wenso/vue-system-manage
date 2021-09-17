68dfa3d1c6f70ea13f0904379870ff07decf2409
os-zhaoyong <zhaoyong.neu@neusoft.com>
2021-09-17 21:45:48 +0800

    调整方法及封装参数

--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -1,17 +1,14 @@
 <template>
   <!--角色列表-->
-  <TableBox v-if="isChild">
-    <span>角色列表</span>
-  </TableBox>
+
   <!--二级路由容器-->
-  <router-view v-else />
+
 </template>
 <script>
-import TableBox from "@comps/templates/TableBox.vue";
+
 import ChildMixin from "@comps/mixins/ChildMixin";
 export default {
   name: "RoleManage",
-  components: {TableBox},
   mixins:[ChildMixin],
   data() {
     return {
